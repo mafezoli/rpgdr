d5c90f5d337ee74e41275b7f7f15275ec613f19c
Guilherme Mafezoli <gmafezoli@protonmail.com>
2021-05-31 23:46:27 -0300

    Update slots for dice buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,56 +16,84 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_d4Button_clicked()
 {
-    auto maxNumber = 5;
-    auto randomNumber = qrand() % maxNumber;
-    ui->valueLabel->setText(QString::number(randomNumber));
+    QString values = "";
+    for(auto value = 0; value < ui->valueSpinBox->value(); value++){
+        auto maxNumber = 5;
+        auto randomNumber = qrand() % maxNumber;
+        values += QString::number(randomNumber) + " ";
+    }
+    ui->valueLabel->setText(values.trimmed());
 }
 
 
 void MainWindow::on_d6Button_clicked()
 {
-    auto maxNumber = 7;
-    auto randomNumber = qrand() % maxNumber;
-    ui->valueLabel->setText(QString::number(randomNumber));
+    QString values = "";
+    for(auto value = 0; value < ui->valueSpinBox->value(); value++){
+        auto maxNumber = 7;
+        auto randomNumber = qrand() % maxNumber;
+        values += QString::number(randomNumber) + " ";
+    }
+    ui->valueLabel->setText(values.trimmed());
 }
 
 
 void MainWindow::on_d8Button_clicked()
 {
-    auto maxNumber = 9;
-    auto randomNumber = qrand() % maxNumber;
-    ui->valueLabel->setText(QString::number(randomNumber));
+    QString values = "";
+    for(auto value = 0; value < ui->valueSpinBox->value(); value++){
+        auto maxNumber = 9;
+        auto randomNumber = qrand() % maxNumber;
+        values += QString::number(randomNumber) + " ";
+    }
+    ui->valueLabel->setText(values.trimmed());
 }
 
 
 void MainWindow::on_d10Button_clicked()
 {
-    auto maxNumber = 11;
-    auto randomNumber = qrand() % maxNumber;
-    ui->valueLabel->setText(QString::number(randomNumber));
+    QString values = "";
+    for(auto value = 0; value < ui->valueSpinBox->value(); value++){
+        auto maxNumber = 11;
+        auto randomNumber = qrand() % maxNumber;
+        values += QString::number(randomNumber) + " ";
+    }
+    ui->valueLabel->setText(values.trimmed());
 }
 
 
 void MainWindow::on_d12Button_clicked()
 {
-    auto maxNumber = 13;
-    auto randomNumber = qrand() % maxNumber;
-    ui->valueLabel->setText(QString::number(randomNumber));
+    QString values = "";
+    for(auto value = 0; value < ui->valueSpinBox->value(); value++){
+        auto maxNumber = 13;
+        auto randomNumber = qrand() % maxNumber;
+        values += QString::number(randomNumber) + " ";
+    }
+    ui->valueLabel->setText(values.trimmed());
 }
 
 
 void MainWindow::on_d20Button_clicked()
 {
-    auto maxNumber = 21;
-    auto randomNumber = qrand() % maxNumber;
-    ui->valueLabel->setText(QString::number(randomNumber));
+    QString values = "";
+    for(auto value = 0; value < ui->valueSpinBox->value(); value++){
+        auto maxNumber = 21;
+        auto randomNumber = qrand() % maxNumber;
+        values += QString::number(randomNumber) + " ";
+    }
+    ui->valueLabel->setText(values.trimmed());
 }
 
 
 void MainWindow::on_d100Button_clicked()
 {
-    auto maxNumber = 101;
-    auto randomNumber = qrand() % maxNumber;
-    ui->valueLabel->setText(QString::number(randomNumber));
+    QString values = "";
+    for(auto value = 0; value < ui->valueSpinBox->value(); value++){
+        auto maxNumber = 101;
+        auto randomNumber = qrand() % maxNumber;
+        values += QString::number(randomNumber) + " ";
+    }
+    ui->valueLabel->setText(values.trimmed());
 }
 
